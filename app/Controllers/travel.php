be2d149a9944cb766b47818b1ed7e44fc0c7597e
chenchangbb <543842130@qq.com>
2020-11-14 23:55:59 +0800

    Add

--- app/Controllers/travel.php
+++ app/Controllers/travel.php
@@ -9,6 +9,45 @@
  // retrieve all the records
  $records = $places->findAll();
  
+ $table = new \CodeIgniter\View\Table();
+ $headings = $places->fields;
+$displayHeadings = array_slice($headings, 1, 2);
+$table->setHeading(array_map('ucfirst', $displayHeadings));
+foreach ($records as $record) {
+$table->addRow($record->name,$record->description);
+}
+return $table->generate();
+
+$nameLink = anchor("travel/showme/$record->id",$record->name);
+$table->addRow($nameLink,$record->description);
+$template = [
+
+'table_open' => '<table cellpadding="5px">',
+
+'cell_start' => '<td style="border: 1px solid #dddddd;">',
+
+'row_alt_start' => '<tr style="background-color:#dddddd">',
+
+];
+
+$table->setTemplate($template);
+$fields = [
+
+'title' => 'Travel Destinations',
+
+'heading' => 'Travel Destinations',
+
+'footer' => 'Copyright Xavier' ];
+
+return $parser->setData($fields)
+
+->render('templates\top') .
+
+$table->generate() .
+
+$parser->setData($fields)
+
+->render('templates\bottom');
  // get a template parser
  $parser = \Config\Services::parser();
  // tell it about the substitions
