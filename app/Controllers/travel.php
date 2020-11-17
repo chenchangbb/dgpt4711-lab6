5e3cdf8e9abfb99f98fefb4502bd8042c6fc0461
chenchangbb <543842130@qq.com>
2020-11-17 21:19:18 +0800

    Add

--- app/Controllers/travel.php
+++ app/Controllers/travel.php
@@ -1,72 +1,79 @@
 <?php
- namespace App\Controllers;
- class Travel extends BaseController
- {
- public function index()
- {
-     // connect to the model
- $places = new \App\Models\Places();
- // retrieve all the records
- $records = $places->findAll();
+
+namespace App\Controllers;
+
  
- $table = new \CodeIgniter\View\Table();
- $headings = $places->fields;
-$displayHeadings = array_slice($headings, 1, 2);
-$table->setHeading(array_map('ucfirst', $displayHeadings));
-foreach ($records as $record) {
-$table->addRow($record->name,$record->description);
+
+class Travel extends BaseController
+{
+
+    public function index()
+    {
+         // connect to the model
+
+        $places = new \App\Models\Places();
+
+            // retrieve all the records
+
+        $records = $places->findAll();
+        
+        $table = new \CodeIgniter\View\Table();
+        $headings = $places->fields;
+        $displayHeadings = array_slice($headings, 1, 2);
+        $table->setHeading(array_map('ucfirst', $displayHeadings));
+        foreach ($records as $record) {
+            $nameLink = anchor("travel/showme/$record->id",$record->name);
+            $table->addRow($nameLink,$record->description,$record->link);
+
 }
-return $table->generate();
-
-$nameLink = anchor("travel/showme/$record->id",$record->name);
-$table->addRow($nameLink,$record->description);
-$template = [
-
+        $template = [
 'table_open' => '<table cellpadding="5px">',
-
 'cell_start' => '<td style="border: 1px solid #dddddd;">',
-
 'row_alt_start' => '<tr style="background-color:#dddddd">',
-
 ];
-
 $table->setTemplate($template);
+
+$parser = \Config\Services::parser(); // tell it about the substitions
+
 $fields = [
+ 'title' => 'Travel Destinations',
+ 'heading' => 'Travel Destinations',
+ 'footer' => 'Copyright Xavier'
+ ];
 
-'title' => 'Travel Destinations',
+        return $parser->setData($fields)
+ ->render('templates\top') .
+ $table->generate() .
+ $parser->setData($fields)
+ ->render('templates\bottom');
+        
+        // get a template parser
 
-'heading' => 'Travel Destinations',
 
-'footer' => 'Copyright Xavier' ];
 
-return $parser->setData($fields)
 
-->render('templates\top') .
+    }
+public function showme($id)
 
-$table->generate() .
+{
+    // connect to the model
 
-$parser->setData($fields)
+$places = new \App\Models\Places();
 
-->render('templates\bottom');
- // get a template parser
- $parser = \Config\Services::parser();
- // tell it about the substitions
- return $parser->setData(['records' => $records])
- // and have it render the template with those
- ->render('placeslist');
- }
- public function showme($id)
- {
-     // connect to the model
- $places = new \App\Models\Places();
- // retrieve all the records
- $record = $places->find($id);
- 
- // get a template parser
- $parser = \Config\Services::parser();
- // tell it about the substitions
- return $parser->setData($record)
- // and have it render the template with those
- ->render('oneplace');
- }
- }
+// retrieve all the records
+
+$record = $places->find($id);
+
+
+
+
+// get a template parser
+
+$parser = \Config\Services::parser(); // tell it about the substitions 
+return $parser->setData($record)
+
+// and have it render the template with those
+
+->render('oneplace');
+}
+}
